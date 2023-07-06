ddb6fb7e8ac04b7758a99c352e71a5d1f4ae8582
vincenzodivita <divitavincenzo@icloud.com>
2023-07-06 18:08:23 +0200

    Add command

--- shell/controllers.sh
+++ shell/controllers.sh
@@ -2,3 +2,6 @@
 
 # Make new PageController inside Guest folder
 php artisan make:controller Guest/PageController
+
+# Make new PageController inside Admin folder
+php artisan make:controller Admin/PageController
