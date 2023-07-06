1bb0b022a3f7aa78791214a1ffa306d6e4a0d93c
vincenzodivita <divitavincenzo@icloud.com>
2023-07-06 17:14:05 +0200

    Install breeze

---
+++ shell/breeze.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+# Install Breeze
+composer require laravel/breeze --dev
+
+# Default scaffolding
+
+php artisan breeze:install
---
+++ shell/laravel.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+# Create a new project with Laravel 9.2s
+composer create-project laravel/laravel:^9.2 .
