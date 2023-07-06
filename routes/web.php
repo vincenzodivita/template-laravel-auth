c16259585801911a52b36793200c86460c9f65f3
vincenzodivita <divitavincenzo@icloud.com>
2023-07-06 18:41:38 +0200

    Add name() and prefix() to group of fn

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,10 @@ Route::get('/', [GuestPageController::class, 'home'])->name('guest.home');
 
 Route::get('/admin', [AdminPageController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('admin.dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')
+->name('admin')
+->prefix('admin')
+->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
