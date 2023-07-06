3125f3ca3405966f5ece43e870fd29a84f372272
vincenzodivita <divitavincenzo@icloud.com>
2023-07-06 19:51:12 +0200

    Add bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+import '~resources/scss/app.scss';
 
-window.Alpine = Alpine;
+import.meta.glob([
+    '../img/**'
+]);
 
-Alpine.start();
+import * as bootstrap from 'bootstrap';
