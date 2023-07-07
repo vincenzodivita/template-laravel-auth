6b24121581b1c449f5c8e6049d8d38665f8f1018
vincenzodivita <divitavincenzo@icloud.com>
2023-07-07 21:27:58 +0200

    Convert from laravel template to empty

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+
+    <div>
+        Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.'
     </div>
 
     <!-- Session Status -->
@@ -10,16 +10,23 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input
+                type="email"
+                class="form-control"
+                id="email"
+                name="email"
+                required
+                autofocus
+                autocomplete="username"
+                value="{{ old('email') }}"
+            >
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div>
+            <button type="submit" class="btn btn-primary">
+                Email Password Reset Link
+            </button>
         </div>
     </form>
-</x-guest-layout>
